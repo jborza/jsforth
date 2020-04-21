d79811feb358310a66d0d15fd59efc3906eb5327
Juraj Borza <juraj.borza@nike.sk>
2020-04-21 23:16:39 +0200

    simple word evaluator, added ?? to output words

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ function createInitialState() {
     return {
         stack: [],
         words: {},
-
-        push: function(x) { return this.stack.push(x)},
-        pop: function() {
+        
+        push: function (x) { return this.stack.push(x) },
+        pop: function () {
             const x = this.stack.pop();
             if (x === undefined) {
                 console.log('Stack underflow');
@@ -16,17 +16,17 @@ function createInitialState() {
 
 function initializeBuiltinWords(state) {
     //a word prototype just operates on the stack 
-    state.words['+'] = (s)=> {state.push(state.pop() + state.pop())};
-    state.words['-'] = (s)=> {state.push(state.pop() - state.pop())};;
-    state.words['*'] = (s) => {state.push(state.pop() * state.pop())};
-    state.words['/'] = (s) => {state.push(state.pop() / state.pop())};
+    state.words['+'] = (s) => { state.push(state.pop() + state.pop()) };
+    state.words['-'] = (s) => { state.push(state.pop() - state.pop()) };;
+    state.words['*'] = (s) => { state.push(state.pop() * state.pop()) };
+    state.words['/'] = (s) => { state.push(state.pop() / state.pop()) };
     state.words['.'] = (s) => { console.log(state.pop()) };
     state.words['dup'] = (s) => {
-         let x = state.pop();
-         state.push(x); 
-         state.push(x); 
-        };
-    state.words['drop'] = (s) => { state.pop()};
+        let x = state.pop();
+        state.push(x);
+        state.push(x);
+    };
+    state.words['drop'] = (s) => { state.pop() };
     state.words['swap'] = (s) => {
         let x2 = state.pop();
         let x1 = state.pop();
@@ -38,7 +38,7 @@ function initializeBuiltinWords(state) {
         let x1 = state.pop();
         state.push(x1);
         state.push(x2);
-        state.push(x1); 
+        state.push(x1);
     }
     state.words['rot'] = (s) => {
         let x3 = state.pop();
@@ -55,28 +55,89 @@ function initializeBuiltinWords(state) {
     state.words['cr'] = (s) => {
         process.stdout.write('\n');
     }
- }
+}
 
 function evaluateLine(state, line) {
-    if(line == '?')
-    {
+    if (line == '?') {
         console.log(state.stack);
         return;
     }
+    if (line == '??') {
+        console.log(state.words);
+    }
     let tokens = line.split(' ');
-    for (token of tokens) {
-        if(evaluateToken(state, token) === false)
+    //feed them into the evaluation function
+    evaluateTokens(state, tokens);
+}
+
+function evaluateTokens(state, tokens) {
+    while (tokens.length > 0) {
+        if (evaluateToken(state, tokens) === false)
             break;
     }
 }
 
-function evaluateToken(state, token) {
+function canAccept(state, token) {
+    if (token in state.words) {
+        return true;
+    }
+    const parsed = parseInt(token);
+    if (isNaN(parsed)) {
+        console.log(token + ' ?');
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function evaluateWordDefinition(state, tokens) {
+    //TODO convert to a function
+    //eat words and store a function definition
+    let body = [];
+    let name = tokens.shift();
+    if (name == undefined)
+        return;
+    while (tokens.length > 0) {
+        token = tokens.shift();
+        if (token == ';') {
+            //end word definition
+            state.words[name] = body;
+            return;
+        }
+        if (canAccept(state, token)) {
+            body.push(token);
+        }
+    }
+}
+
+function evaluateToken(state, tokens) {
+    token = tokens.shift();
     //When the interpreter finds a word, it looks the word up in the dictionary.
     if (token in state.words) {
         //If the word is found, the interpreter executes the code associated with the word, and then returns to parse the rest of the input stream. 
-        state.words[token](state);
+        word = state.words[token];
+        if (typeof (word) === 'function') {
+            state.words[token](state);
+        }
+        else {
+            evaluateTokens(state, word);
+        }
         return;
     }
+
+    //special words
+    //TODO definitions
+    //TODO conditionals: if ... then | if ... else ... then
+    // -  Conditionals in Forth can only be used inside definitions. 
+    //DO .. LOOP: remember that the words DO and LOOP are branching commands and that therefore they can only be executed inside a definition.
+    //+LOOP pops the increment from the stack, e.g. 0 10 do i . -1 +loop
+
+    if (token == ':') {
+        evaluateWordDefinition(state, tokens);
+        return;
+    }
+
     //If the word isn't found, the word is assumed to be a number and an attempt is made to convert it into a number and push it on the stack;
     const parsed = parseInt(token);
     if (isNaN(parsed)) {
